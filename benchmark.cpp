d9dcf906953e25dc605f017686564f8efc655bb9
saliyvlad <saliy.vlad.jr@gmail.com>
2026-06-05 11:38:45 +0700

    Github actions

--- benchmark.cpp
+++ benchmark.cpp
@@ -131,8 +131,8 @@ public:
 
 int main() {
     TRMMBenchmark benchmark;
-    size_t m = -21;
-    size_t n = -123;
+    size_t m = 256;
+    size_t n = 256;
     int iterations = 10;
     
     std::cout << "TRMM Performance Benchmark\n";
